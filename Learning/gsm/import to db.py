72de8d08547466a11ec3f875147ec86612d7ce6c
tansquire <34076657+tansquire@users.noreply.github.com>
2018-02-17 06:42:00 +0530

    Update import to db.py

--- Learning/gsm/import to db.py
+++ Learning/gsm/import to db.py
@@ -3,35 +3,12 @@ import serial
 import time
 import mysql.connector
 from time import gmtime, strftime
-ser = serial.Serial('/dev/ttyUSB0', baudrate = 9600)
+from datetime import datetime
+ser = serial.Serial('/dev/ttyACM0', baudrate = 9600)
 time.sleep(1)
 start = time.time()
 
-def do():
- conn=mysql.connector.connect(user='root',password='gowsalya',host='10.21.160.201',database='scada')
- mycursor=conn.cursor()
- if(ser.inWaiting()>0):
-  #data=ser.readline().decode().split('\r\n')
-  data=ser.readline();
-  #print(data[0])
-  print(data)
-  #if('deviceA' in data[0]):
-   #mycursor.execute("UPDATE GSMAI SET value='%s'WHERE id='%s'" % (data[0][9:], 1))
-   #print"lakewell data=%s"%(data[0][9:])
-  #if('deviceB' in data[0]):
-   #mycursor.execute("UPDATE GSMAI SET value='%s'WHERE id='%s'" % (data[0][9:], 2))
-   #print"childrenpark data=%s"%(data[0][9:])
- 
-
- conn.commit()
- conn.close()
- mycursor.close() 
- return
-
 while(1):
- 
-
- 
  conn=mysql.connector.connect(user='root',password='gowsalya',host='10.21.160.201',database='scada')
  mycursor=conn.cursor()
  mycursor.execute("select * FROM DI where id=3") 
@@ -48,7 +25,7 @@ while(1):
  mycursor.close()
  end = time.time()
  if(end - start>300 and lake_well_lora_status=='0'):
-  print strftime("%Y-%m-%d %H:%M:%S", gmtime())
+  print datetime.now()
   time.sleep(.1)
   ser.write(b'a')
   start=end;
@@ -56,14 +33,48 @@ while(1):
  #if(lake_well_lora_status=='0'):
   #ser.write(b'a')
   #print ("lake well lora fail, command given to lake well")
- do()
+ conn=mysql.connector.connect(user='root',password='gowsalya',host='10.21.160.201',database='scada')
+ mycursor=conn.cursor()
+ if(ser.inWaiting()>0):
+  data=ser.readline().decode().split('\r\n')
+  #data=ser.readline();
+  print(data[0])
+  #print(data)
+  #if('deviceA' in data[0] and 'p' in data[0] and 'q' in data[0] and '<' in data[0] and '>' in data[0]):   
+   #mycursor.execute("UPDATE GSMAI SET value='%s'WHERE id='%s'" % (data[0][data[0].find('<')+1:data[0].find('>')], 1))
+   #print"lakewell data=%s"%(data[0][data[0].find('<')+1:data[0].find('>')])
+  #if('deviceB' in data[0] and 'p' in data[0] and 'q' in data[0] and '<' in data[0] and '>' in data[0]):
+   #mycursor.execute("UPDATE GSMAI SET value='%s'WHERE id='%s'" % (data[0][data[0].find('<')+1:data[0].find('>')], 2))
+   #print"lakewell data=%s"%(data[0][data[0].find('<')+1:data[0].find('>')])
+
+  if('deviceA' in data[0] and 'p' in data[0] and 'q' in data[0] and '<' in data[0] and '>' in data[0]):   
+   mycursor.execute("insert into gsm_test (id, name, value) values (%d, '%s', '%s')" % (1, datetime.now(),data[0]))
+   
+  if('Non-sense' in data[0]):   
+   mycursor.execute("insert into gsm_test (id, name,value) values (%d, '%s', '%s')" % (1, datetime.now(),data[0]))
+   
+  if('modem' in data[0]):   
+   mycursor.execute("insert into gsm_test (id, name,value) values (%d, '%s', '%s')" % (1, datetime.now(),data[0])) 
+
+  if('Sending' in data[0]):   
+   mycursor.execute("insert into gsm_test (id, name,value) values (%d, '%s', '%s')" % (1, datetime.now(),data[0]))  
+   
+  
+
+
+   #mycursor.execute("UPDATE GSMAI SET value='%s'WHERE id='%s'" % (data[0][data[0].find('<')+1:data[0].find('>')], 1))
+   #print"lakewell data=%s"%(data[0][data[0].find('<')+1:data[0].find('>')])
+  #if('deviceB' in data[0] and 'p' in data[0] and 'q' in data[0] and '<' in data[0] and '>' in data[0]):
+   #mycursor.execute("UPDATE GSMAI SET value='%s'WHERE id='%s'" % (data[0][data[0].find('<')+1:data[0].find('>')], 2))
+   #print"lakewell data=%s"%(data[0][data[0].find('<')+1:data[0].find('>')])
+
+ conn.commit()
+ conn.close()
+ mycursor.close() 
  #time.sleep(300)
 
  #if(childrenpark_lora_status=='0'):
   #ser.write(b'b')
   #print ("children park lora fail, command given to children park")
   #do()
- #time.sleep(60)
-
-
-
+#time.sleep(60)
