adc042cd48da75a09784a0707b29b3d3e4bf9307
tansquire <34076657+tansquire@users.noreply.github.com>
2018-02-28 11:06:13 +0530

    Update comm_status_from_random_value.py

--- Learning/gsm/comm_status_from_random_value.py
+++ Learning/gsm/comm_status_from_random_value.py
@@ -1,44 +1,66 @@
 #!/usr/bin/python
 import time
 import mysql.connector
-import random
-#data=""
-#data='$4#26.2'                    #for stuff club             
-#data='$3#26.2#4#10.2'             #stuf club and children park
-data='$2#30.2#3#26.2#4#70.2'      # for all
+new_stuf_club_value=new_childrenpark_value=new_lakewell_value=new_RR_sump_value=new_actuator_value=old_stuf_club_value=old_childrenpark_value=old_lakewell_value=old_RR_sump_value=old_actuator_value=0
+stuff_club_comm_status=childrenpark_comm_status=lakewell_comm_status=RR_sump_comm_status=actuator_comm_status=0
 
-def randomvalue():
- if len(data)!=0:
-  conn=mysql.connector.connect(user='root',password='gowsalya',host='10.21.160.201',database='scada')
-  mycursor=conn.cursor()
-  if data[1]=='4':
-   x=random.randint(1, 500)
-   mycursor.execute("UPDATE AI SET value='%s'WHERE id='%s'" % (x, 7))
-   print"stuf club lora available, random value=%s"%x
-  elif data[1]=='3':
-   x=random.randint(1, 500)
-   y=random.randint(1, 500)
-   mycursor.execute("UPDATE AI SET value='%s'WHERE id='%s'" % (x, 6))
-   mycursor.execute("UPDATE AI SET value='%s'WHERE id='%s'" % (y, 7))
-   print"stuf club and children park lora available, random value=%s and %s"%(x,y)
-  elif data[1]=='2':
-   x=random.randint(1, 500)
-   y=random.randint(1, 500)
-   z=random.randint(1, 500)
-   mycursor.execute("UPDATE AI SET value='%s'WHERE id='%s'" % (x, 5))
-   mycursor.execute("UPDATE AI SET value='%s'WHERE id='%s'" % (y, 6))
-   mycursor.execute("UPDATE AI SET value='%s'WHERE id='%s'" % (z, 7))
-   print"All lora available, random values=%s, %s and %s"%(x,y,z)
-  conn.commit()
-  conn.close()
-  mycursor.close()
+def getvalue(i):
+ conn=mysql.connector.connect(user='root',password='gowsalya',host='10.21.160.201',database='scada')
+ mycursor=conn.cursor()
+ mycursor.execute("select * FROM AI where id=%s",(i,)) 
+ list=mycursor.fetchall()
+ return (list[0][2])
+ conn.close()
+ mycursor.close()
+
+while(1):
+
+ new_stuf_club_value=getvalue(7)
+ new_childrenpark_value=getvalue(6)
+ new_lakewell_value=getvalue(5)
+ new_RR_sump_value=getvalue(8)
+ new_actuator_value=getvalue(9)
+ 
+ if (new_stuf_club_value != old_stuf_club_value):
+  stuff_club_comm_status=1
+  old_stuf_club_value = new_stuf_club_value
  else:
-  return
- return
+  stuff_club_comm_status=0
+ 
+ if (new_childrenpark_value != old_childrenpark_value):
+  childrenpark_comm_status=1
+  old_childrenpark_value = new_childrenpark_value
+ else:
+  childrenpark_comm_status=0
 
+ if (new_lakewell_value != old_lakewell_value):
+  lakewell_comm_status=1
+  old_lakewell_value = new_lakewell_value
+ else:
+  lakewell_comm_status=0
 
+ if (new_RR_sump_value != old_RR_sump_value):
+  RR_sump_comm_status=1
+  old_RR_sump_value = new_RR_sump_value
+ else:
+  RR_sump_comm_status=0
 
-
-while(1):                             
- randomvalue()
-time.sleep(1)
+ if (new_actuator_value != old_actuator_value):
+  actuator_comm_status=1
+  old_actuator_value = new_actuator_value
+ else:
+  actuator_comm_status=0
+ 
+ conn=mysql.connector.connect(user='root',password='gowsalya',host='10.21.160.201',database='scada')
+ mycursor=conn.cursor()
+ mycursor.execute("UPDATE DI SET value='%s'WHERE id='%s'" % (lakewell_comm_status, 3))
+ mycursor.execute("UPDATE DI SET value='%s'WHERE id='%s'" % (childrenpark_comm_status, 4))
+ mycursor.execute("UPDATE DI SET value='%s'WHERE id='%s'" % (stuff_club_comm_status, 5))
+ mycursor.execute("UPDATE DI SET value='%s'WHERE id='%s'" % (RR_sump_comm_status, 6))
+ mycursor.execute("UPDATE DI SET value='%s'WHERE id='%s'" % (actuator_comm_status, 7))
+ print"lakewell, children, stuff_club, RR sump, actuator comm availabilities respectively are=%s, %s, %s, %s and %s"%(lakewell_comm_status,childrenpark_comm_status,stuff_club_comm_status, RR_sump_comm_status, actuator_comm_status)
+ conn.commit()
+ conn.close()
+ mycursor.close()
+ time.sleep(3)   
+#this time must be greater than gate random update time(so gateway freq must be known)
