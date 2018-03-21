6b41333b918ca96096864e16d6d0a1bf73a15b7e
tansquire <34076657+tansquire@users.noreply.github.com>
2018-03-21 14:28:36 +0530

    Update server_side_read.ino

--- Learning/gsm/server_side_read.ino
+++ Learning/gsm/server_side_read.ino
@@ -1,11 +1,13 @@
 #include "SIM900.h"
 #include <SoftwareSerial.h>
 #include "sms.h"
+//SoftwareSerial Serial1(2, 3);
 SMSGSM sms;
 int numdata;
 boolean started=false;
 char smsbuffer[160];
 char n[20];
+char x;
 
 
 void setup()
@@ -13,7 +15,7 @@ void setup()
     
      Serial.begin(9600);
      Serial.println("GSM Shield testing.");
-     if (gsm.begin(9600)) 
+     if (gsm.begin(2400)) 
      {
           Serial.println("\nstatus=READY");
           started=true;
@@ -35,8 +37,19 @@ void loop()
      {
   
         gsm.readSMS(smsbuffer, 160, n, 20);
-       
-        sms.DeleteSMS(1);
+        if(strstr(smsbuffer,"@")!=NULL && strstr(smsbuffer,"#")!=NULL)
+        {
+        *smsbuffer='\0';
+        Serial.println("got expected");
+        Serial.println("going to delete");
+        x=sms.DeleteSMS(1);
+        if(x==1)
+        Serial.println("deleted");
+        }
+        
+        *smsbuffer='\0';
+
+        
     
      }
 }
