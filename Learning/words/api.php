ea8e41c18e94fec06d6d40427e9cc1eed35de610
tansquire <34076657+tansquire@users.noreply.github.com>
2019-08-28 23:01:07 +0530

    initial commit

---
+++ Learning/words/api.php
@@ -0,0 +1,124 @@
+<?php 
+
+	class Api extends Rest {
+		
+		public function __construct() {
+			parent::__construct();
+		}
+
+		public function generateToken() {
+			$email = $this->validateParameter('email', $this->param['email'], STRING);
+			$pass = $this->validateParameter('pass', $this->param['pass'], STRING);
+			try {
+				$stmt = $this->dbConn->prepare("SELECT * FROM users WHERE email = :email AND password = :pass");
+				$stmt->bindParam(":email", $email);
+				$stmt->bindParam(":pass", $pass);
+				$stmt->execute();
+				$user = $stmt->fetch(PDO::FETCH_ASSOC);
+				if(!is_array($user)) {
+					$this->returnResponse(INVALID_USER_PASS, "Email or Password is incorrect.");
+				}
+
+				if( $user['active'] == 0 ) {
+					$this->returnResponse(USER_NOT_ACTIVE, "User is not activated. Please contact to admin.");
+				}
+
+				$paylod = [
+					'iat' => time(),
+					'iss' => 'localhost',
+					'exp' => time() + (15*60),
+					'userId' => $user['id']
+				];
+
+				$token = JWT::encode($paylod, SECRETE_KEY);
+				
+				$data = ['token' => $token];
+				$this->returnResponse(SUCCESS_RESPONSE, $data);
+			} catch (Exception $e) {
+				$this->throwError(JWT_PROCESSING_ERROR, $e->getMessage());
+			}
+		}
+
+		public function addWord() {
+			$pos1 = $this->validateParameter('pos1', $this->param['pos1'], STRING, false);
+			$word = $this->validateParameter('word', $this->param['word'], STRING, false);
+			$sentence = $this->validateParameter('sentence', $this->param['sentence'], STRING, false);
+			$meaning = $this->validateParameter('meaning', $this->param['meaning'], STRING, false);
+
+			$word_obj = new Word;
+			$word_obj->setPos($pos1);
+			$word_obj->setWord($word);
+			$word_obj->setSentence($sentence);
+			$word_obj->setMeaning($meaning);
+			$word_obj->setCreatedBy($this->userId);
+			$word_obj->setCreatedOn(date('Y-m-d'));
+
+			if(!$word_obj->insert()) {
+				$message = 'Failed to insert.';
+			} else {
+				$message = "Inserted successfully.";
+			}
+
+			$this->returnResponse(SUCCESS_RESPONSE, $message);
+		}
+
+		public function getWordDetails() {
+			$wordId = $this->validateParameter('wordId', $this->param['wordId'], INTEGER);
+
+			$word_obj = new Word;
+			$word_obj->setId($wordId);
+			$word = $word_obj->getWordDetailsById();
+			if(!is_array($word)) {
+				$this->returnResponse(SUCCESS_RESPONSE, ['message' => 'Word details not found.']);
+			}
+
+			$response['wordId'] 		= $word['id'];
+			$response['wordName'] 		= $word['pos1'];
+			$response['word'] 			= $word['word'];
+			$response['meaning'] 		= $word['meaning'];
+			$response['sentence'] 		= $word['sentence'];
+			$response['createdBy'] 		= $word['created_user'];
+			$response['lastUpdatedBy'] 	= $word['updated_user'];
+			$this->returnResponse(SUCCESS_RESPONSE, $response);
+		}
+
+		public function updateWord() {
+			$wordId = $this->validateParameter('wordId', $this->param['wordId'], INTEGER);
+			$pos1 = $this->validateParameter('pos1', $this->param['pos1'], STRING, false);
+			$sentence = $this->validateParameter('sentence', $this->param['sentence'], STRING, false);
+			$meaning = $this->validateParameter('meaning', $this->param['meaning'], STRING, false);
+
+			$word_obj = new Word;
+			$word_obj->setId($wordId);
+			$word_obj->setPos($pos1);
+			$word_obj->setSentence($sentence);
+			$word_obj->setMeaning($meaning);
+			$word_obj->setUpdatedBy($this->userId);
+			$word_obj->setUpdatedOn(date('Y-m-d'));
+
+			if(!$word_obj->update()) {
+				$message = 'Failed to update.';
+			} else {
+				$message = "Updated successfully.";
+			}
+
+			$this->returnResponse(SUCCESS_RESPONSE, $message);
+		}
+
+		public function deleteWord() {
+			$wordId = $this->validateParameter('wordId', $this->param['wordId'], INTEGER);
+
+			$word_obj = new Word;
+			$word_obj->setId($wordId);
+
+			if(!$word_obj->delete()) {
+				$message = 'Failed to delete.';
+			} else {
+				$message = "deleted successfully.";
+			}
+
+			$this->returnResponse(SUCCESS_RESPONSE, $message);
+		}
+	}
+	
+ ?>
---
+++ Learning/words/word.php
@@ -0,0 +1,127 @@
+<?php 
+	class Word {
+		private $id;
+		private $pos1;
+		private $word;
+		private $sentence;
+		private $meaning;
+		private $updatedBy;
+		private $updatedOn;
+		private $createdBy;
+		private $createdOn;
+		private $tableName = 'wordlist';
+		private $dbConn;
+
+		function setId($id) { $this->id = $id; }
+		function getId() { return $this->id; }
+		function setPos($pos1) { $this->pos1 = $pos1; }
+		function getPos() { return $this->pos1; }
+		function setWord($word) { $this->word = $word; }
+		function getWord() { return $this->word; }
+		function setSentence($sentence) { $this->sentence = $sentence; }
+		function getSentence() { return $this->sentence; }
+		function setMeaning($meaning) { $this->meaning = $meaning; }
+		function getMeaning() { return $this->meaning; }
+		function setUpdatedBy($updatedBy) { $this->updatedBy = $updatedBy; }
+		function getUpdatedBy() { return $this->updatedBy; }
+		function setUpdatedOn($updatedOn) { $this->updatedOn = $updatedOn; }
+		function getUpdatedOn() { return $this->updatedOn; }
+		function setCreatedBy($createdBy) { $this->createdBy = $createdBy; }
+		function getCreatedBy() { return $this->createdBy; }
+		function setCreatedOn($createdOn) { $this->createdOn = $createdOn; }
+		function getCreatedOn() { return $this->createdOn; }
+
+		public function __construct() {
+			$db = new DbConnect();
+			$this->dbConn = $db->connect();
+		}
+
+		public function getAllWords() {
+			$stmt = $this->dbConn->prepare("SELECT * FROM " . $this->tableName);
+			$stmt->execute();
+			$words = $stmt->fetchAll(PDO::FETCH_ASSOC);
+			return $words;
+		}
+
+		public function getWordDetailsById() {
+
+			$sql = "SELECT 
+						w.*, 
+						u.name as created_user,
+						u1.name as updated_user
+					FROM wordlist w 
+						JOIN users u ON (w.created_by = u.id) 
+						LEFT JOIN users u1 ON (w.updated_by = u1.id) 
+					WHERE 
+						w.id = :wordId";
+
+			$stmt = $this->dbConn->prepare($sql);
+			$stmt->bindParam(':wordId', $this->id);
+			$stmt->execute();
+			$word = $stmt->fetch(PDO::FETCH_ASSOC);
+			return $word;
+		}
+		
+
+		public function insert() {
+			
+			$sql = 'INSERT INTO ' . $this->tableName . '(id, pos1, word, sentence, meaning, created_by, created_on) VALUES(null, :pos1, :word, :sentence, :meaning, :createdBy, :createdOn)';
+
+			$stmt = $this->dbConn->prepare($sql);
+			$stmt->bindParam(':pos1', $this->pos1);
+			$stmt->bindParam(':word', $this->word);
+			$stmt->bindParam(':sentence', $this->sentence);
+			$stmt->bindParam(':meaning', $this->meaning);
+			$stmt->bindParam(':createdBy', $this->createdBy);
+			$stmt->bindParam(':createdOn', $this->createdOn);
+			
+			if($stmt->execute()) {
+				return true;
+			} else {
+				return false;
+			}
+		}
+
+		public function update() {
+			
+			$sql = "UPDATE $this->tableName SET";
+			if( null != $this->getPos()) {
+				$sql .=	" pos1 = '" . $this->getPos() . "',";
+			}
+
+			if( null != $this->getSentence()) {
+				$sql .=	" sentence = '" . $this->getSentence() . "',";
+			}
+
+			if( null != $this->getMeaning()) {
+				$sql .=	" meaning = " . $this->getMeaning() . ",";
+			}
+
+			$sql .=	" updated_by = :updatedBy, 
+					  updated_on = :updatedOn
+					WHERE 
+						id = :userId";
+
+			$stmt = $this->dbConn->prepare($sql);
+			$stmt->bindParam(':userId', $this->id);
+			$stmt->bindParam(':updatedBy', $this->updatedBy);
+			$stmt->bindParam(':updatedOn', $this->updatedOn);
+			if($stmt->execute()) {
+				return true;
+			} else {
+				return false;
+			}
+		}
+
+		public function delete() {
+			$stmt = $this->dbConn->prepare('DELETE FROM ' . $this->tableName . ' WHERE id = :userId');
+			$stmt->bindParam(':userId', $this->id);
+			
+			if($stmt->execute()) {
+				return true;
+			} else {
+				return false;
+			}
+		}
+	}
+ ?>
