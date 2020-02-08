f1cf2350e02f2dcf8f1debbdb61d1cc693b92923
tansquire <34076657+tansquire@users.noreply.github.com>
2020-02-08 17:29:03 +0530

    initial commit

---
+++ ACS/ssh.py
@@ -0,0 +1,10 @@
+import paramiko
+ssh = paramiko.SSHClient()
+ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+ssh.connect('159.65.149.0', username='dadu', password='0trina0partha', key_filename='/home/pi/.ssh/id_rsa')
+ftp = ssh.open_sftp()
+file=ftp.file('/home/dadu/partha.txt', "w", -1)
+file.write('Hello World!')
+file.flush()
+ftp.close()
+ssh.close()
