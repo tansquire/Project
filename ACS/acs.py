714e198baea4f22778c2e7ce2a314b5f3c24091c
tansquire <34076657+tansquire@users.noreply.github.com>
2020-02-08 17:33:52 +0530

    Add files via upload

---
+++ ACS/acs.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python
+import serial
+import time
+import paramiko
+from datetime import datetime
+from datetime import date
+while(1):
+ try:
+  ser = serial.Serial('/dev/ttyACM0', baudrate = 9600)
+  data=ser.readline().decode().split('\r\n')
+  x=str(data)
+  if(('Tag' in x) and len(data[0])==23):
+   now = datetime.now()
+   today = date.today()
+   time1 = now.strftime("%H:%M:%S")
+   date1 = today.strftime("%d/%m/%Y")
+   print ("Getting data ...")
+   #print data[0][-8:]
+   send_data=date1+'p'+data[0][-8:]+'q'+time1
+   print send_data
+   #print date1
+   #print time1
+   ssh = paramiko.SSHClient()
+   ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+   ssh.connect('159.65.149.0', username='dadu', password='0trina0partha', key_filename='/home/pi/.ssh/id_rsa')
+   ftp = ssh.open_sftp()
+   file=ftp.file('/home/dadu/partha.txt', "w", -1)
+   file.write(send_data)
+   file.flush()
+   ftp.close()
+   ssh.close()
+   print("data updated in the server")
+  #time.sleep(0.01)
+ except:
+  print("error")
+ time.sleep(0.01)
